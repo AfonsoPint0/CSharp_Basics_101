5bc1f5c7d1c44feb49f20f8c0194cef1b6acfb35
AfonsoPint0 <afonso.pinto@fegime.pt>
2022-07-01 18:15:55 +0100

    Simple Bank

--- CSharp_Basics_101/Program.cs
+++ CSharp_Basics_101/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharp_Basics_101;
+
+var account = new BankAccount("Kendra", 1000);
+Console.WriteLine($"Account: {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+account.MakeWhitdrawal(120, DateTime.Now, "Hammock");
+Console.WriteLine(account.Balance);
+
+account.MakeWhitdrawal(50, DateTime.Now, "Xbox game");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.getAccountHistory());
+
+
+// Test that the initial balances must be positive
+try
+{
+    account.MakeDeposit(300, DateTime.Now, "stealing");
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught, creating account with negative balance");
+    Console.WriteLine(e.ToString());
+}
+
+
